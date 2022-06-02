48bb9baac085b6c7d3006832eb76d38799e58676
Manish <manishprajapatim416@gmail.com>
2022-06-02 21:27:37 +0530

    .

--- src/components/App.js
+++ src/components/App.js
@@ -1,18 +1,177 @@
-import React from 'react'
-import '../styles/App.css';
-import like from '../like.svg';
-const App = () => {
-const [count,setCount] = React.useState(0)
-const handleClick = () =>{
-  setCount(count=>count+1)
-}
+import React, { Component, useState } from "react";
+import "./../styles/App.css";
+import { StateDetails } from "./State";
+
+// Do not alter the states const and values inside it.
+const states = [
+  {
+    name: "Madhya Pradesh",
+    cities: [
+      {
+        name: "Indore",
+        towns: [
+          {
+            name: "Mhow",
+          },
+          {
+            name: "Dewas",
+          },
+        ],
+      },
+      {
+        name: "Bhopal",
+        towns: [
+          {
+            name: "Manit",
+          },
+          {
+            name: "Berasia",
+          },
+        ],
+      },
+      {
+        name: "Gwalior",
+        towns: [
+          {
+            name: "Ajaypur",
+          },
+        ],
+      },
+    ],
+  },
+  {
+    name: "Jharkhand",
+    cities: [
+      {
+        name: "Dhanbad",
+        towns: [
+          {
+            name: "IIT(ISM) Dhanbad",
+          },
+          {
+            name: "Hirapur",
+          },
+        ],
+      },
+      {
+        name: "Wasseypur",
+        towns: [
+          {
+            name: "Sardar khan's",
+          },
+          {
+            name: "Faizal khan's",
+          },
+        ],
+      },
+      {
+        name: "Mirzapur",
+        towns: [
+          {
+            name: "Kaleen bhaiya's",
+          },
+          {
+            name: "Guddu bhaiya's",
+          },
+        ],
+      },
+    ],
+  },
+  {
+    name: "Assam",
+    cities: [
+      {
+        name: "Guwhati",
+        towns: [
+          {
+            name: "Amin",
+          },
+          {
+            name: "Jalah",
+          },
+        ],
+      },
+      {
+        name: "Jungle1",
+        towns: [
+          {
+            name: "Tiger found at IIT Guwahati",
+          },
+          {
+            name: "Leopard found in IIT Guwahati",
+          },
+        ],
+      },
+      {
+        name: "Tezpur",
+        towns: [
+          {
+            name: "Dibrugarh",
+          },
+          {
+            name: "Silchar",
+          },
+        ],
+      },
+    ],
+  },
+  {
+    name: "Bihar",
+    cities: [
+      {
+        name: "Patna",
+        towns: [
+          {
+            name: "Sonpur",
+          },
+          {
+            name: "Maner",
+          },
+        ],
+      },
+      {
+        name: "Gaya",
+        towns: [
+          {
+            name: "Bakraur",
+          },
+          {
+            name: "Barachatti",
+          },
+        ],
+      },
+      {
+        name: "Darbhanga",
+        towns: [
+          {
+            name: "Singhwara",
+          },
+          {
+            name: "Jale",
+          },
+        ],
+      },
+    ],
+  },
+];
+
+function App() {
   return (
     <div id="main">
-      <img id="like-btn-img" src={like} style={{fill:'white',width:'100px',backgroundColor:`rgba(255,0,0,${0.1*count})`}} onClick={handleClick}/>
-      <h3>Likes: <span id="like-counter">{count}</span></h3>
+      <div>
+        {states.map((stateName, stateId) => {
+          return (
+            <StateDetails
+              stateVar={stateName}
+              stateName={stateName.name}
+              stateId={stateId}
+              key={stateId}
+            />
+          );
+        })}
+      </div>
     </div>
-  )
+  );
 }
-
 
 export default App;
